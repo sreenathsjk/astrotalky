35f6a7fbd4040334f829f4e58ebc73838e2eee60
sreenathsjk <content2u.sj@gmail.com>
2026-03-24 12:08:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+ const firebaseConfig = {
+  apiKey: "AIzaSyCMcu_iTxeweljHGn5EiZTwms0eH_iLj3Y",
+  authDomain: "astrotalki-a75ea.firebaseapp.com",
+  projectId: "astrotalki-a75ea",
+  storageBucket: "astrotalki-a75ea.firebasestorage.app",
+  messagingSenderId: "849861088684",
+  appId: "1:849861088684:web:9b3e0d6540478f2121d609"
 };
 
 const app = initializeApp(firebaseConfig);
