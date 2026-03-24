af8444873fd8b1012860ea6abbaecd3fa50c630e
sreenathsjk <content2u.sj@gmail.com>
2026-03-24 12:03:56 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
+  apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT.firebaseapp.com",
   projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
